a7b7d01280132e206d25bad65f20e9d6f4894200
Korawich2549 <jamekunza123@gmail.com>
2025-08-22 11:13:53 +0700

    test08 22/8

---
+++ test01.py
@@ -0,0 +1,15 @@
+#single Line Coment
+print('Hello Korawich') #Wow wow wow
+
+#Multi Line Coment
+''' ใฃ้ Triple Single Quote
+    asdfsdf
+    sdfsdf
+    sdfsdfsdfsds
+'''
+
+""" ใฃ้ Triple Double Quote
+    asdfsdf
+    sdfsdf
+    sdfsdfsdfsds
+"""
---
+++ test03.py
@@ -0,0 +1,18 @@
+# คำสั่งแสดงผลข้อมูลใดๆ ทางหน้าจอ ใฃ้ฟังก์ชัน print()
+# string
+print('Hello Korawich')
+print("Hi Korawich")
+print('''Hey''')
+print("""Hi Korawich""")
+
+# number
+print(1234567)   # integer/int
+print(3.14159)   # float
+
+# boolean ตรรกะ มีเเค่ True และ False
+print(True)
+print(False)
+
+# Expression นิพจน์ คือ อะไรก็ตามที่มี process การทำงาน เช่น การคำนวณ, ตัวแปร, ...
+print(10+20+50)
+print(10*2/2)
