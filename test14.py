827230040562d5ffd404bcc428190580be0c2397
Korawich2549 <jamekunza123@gmail.com>
2025-08-29 11:13:55 +0700

    test11-17 29/8

---
+++ test14.py
@@ -0,0 +1,15 @@
+# if-elif-else
+ 
+score = 73
+ 
+if score >= 80 :
+    print("A")
+elif score >= 70 :
+    print("B")
+elif score >= 60 :
+    print("C")
+elif score >= 50 :
+    print("D")
+else :
+    print("F")
+print('Good luck')
